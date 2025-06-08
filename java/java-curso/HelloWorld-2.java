3c747445b90abaca0df62719511b6d0c372e06cd
dvasco <dvasco@gmail.com>
2025-06-08 12:35:24 +0200

    savig whole files

---
+++ java/java-curso/HelloWorld-2.java
@@ -0,0 +1,5 @@
+public class HelloWorld-2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ java/java-mis-proyectos/calcIP.java
@@ -0,0 +1,9 @@
+public class calcIP {
+
+ 
+    public  static void main(String[] args) {
+
+       ipcalc128 ipc = new ipcalc128();
+
+   }
+}
