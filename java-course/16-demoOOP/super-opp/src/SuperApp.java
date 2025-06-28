7eb613a8b5bdf3dc6c79277659b786620cda8ce4
dvasco <dvasco@gmail.com>
2025-06-28 13:27:09 +0200

    new test on SuperApp.java

--- java-course/16-demoOOP/super-opp/src/SuperApp.java
+++ java-course/16-demoOOP/super-opp/src/SuperApp.java
@@ -1,9 +1,5 @@
 
-<<<<<<< HEAD:java-course/16-demoOOP/super-opp/src/SuperApp.java
-public class SuperApp {// test!!!!
-=======
-public class SuperApp {//test
->>>>>>> 73bcd88b7c2c848bbbf3f116800558e26474e871:java-course/16-demo/super-opp/src/SuperApp.java
+public class SuperApp {// new test
     public static void main(String[] args) throws Exception {
         System.out.println("Bienvenido a nuestro supermercado onLine");
     }
