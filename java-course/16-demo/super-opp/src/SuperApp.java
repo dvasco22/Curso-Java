73bcd88b7c2c848bbbf3f116800558e26474e871
dvasco22 <119160757+dvasco22@users.noreply.github.com>
2025-06-28 13:13:55 +0200

    Update SuperApp.java

--- java-course/16-demo/super-opp/src/SuperApp.java
+++ java-course/16-demo/super-opp/src/SuperApp.java
@@ -1,5 +1,5 @@
 
-public class SuperApp {
+public class SuperApp {//test
     public static void main(String[] args) throws Exception {
         System.out.println("Bienvenido a nuestro supermercado onLine");
     }
